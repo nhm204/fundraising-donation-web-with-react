7decc289e98ca18c26822a58039af2ee0cf5e0f5
nhm204 <19521862@gm.uit.edu.vn>
2022-11-10 08:39:57 +0700

    fix recent search feature

--- src/pages/components/DropdownSearch/DropdownSearch.jsx
+++ src/pages/components/DropdownSearch/DropdownSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import './DropdownSearch.scss';
 import { BsSearch } from 'react-icons/bs';
@@ -9,23 +9,23 @@ import Project from '../Projects/ProjectCard/ProjectCard';
 
 const DropdownSearch = ({ setSearchQuery, searchValue, setSelectedCategory, setCurrentPage, setIsSelected, link }) => {
   const [ inputValue, setInputValue ] = useState('');
-  const [ searchHistory, setSearchHistory ] = useState([]);
   const [ searchParams, setSearchParams ] = useSearchParams();
   const navigate = useNavigate();
-  const searchHistoryRef = useRef();
-
-  useEffect(() => {
-    searchHistoryRef.current = searchHistory;
-  }, [searchHistory]); 
 
 
   const handleDirect = (e) => {
     if (e.key === 'Enter') {
       if (e.target.value !== '') {
         localStorage.setItem('searchValue', e.target.value);
+
         let history = JSON.parse(localStorage.getItem('history') || '[]'); // convert string to object
-        history.push(inputValue);
+        history.unshift(inputValue?.toLowerCase());
+        let sameSearchIndex = history?.indexOf(inputValue?.toLowerCase(), 1);
+        if (sameSearchIndex !== -1) {
+          history.splice(sameSearchIndex, 1);
+        }
         localStorage.setItem('history', JSON.stringify(history)); // convert object to string
+
         navigate({ pathname: '/discover', search: `?search=${e.target.value}`});
         setIsSelected(false);
       }
@@ -33,6 +33,7 @@ const DropdownSearch = ({ setSearchQuery, searchValue, setSelectedCategory, setC
     }
   }
 
+  
   useEffect(() => {
     setInputValue(searchValue);
   }, [searchValue]);
@@ -64,28 +65,13 @@ const DropdownSearch = ({ setSearchQuery, searchValue, setSelectedCategory, setC
 
   let searchList = useMemo(() => {
     if (inputValue) {
-      return projects?.filter(project => project.name.toLowerCase().includes(inputValue) || project.category.toLowerCase().includes(inputValue)).slice(0, 3); 
+      return projects?.filter(project => project.name.toLowerCase().includes(inputValue.toLocaleLowerCase()) || project.category.toLowerCase().includes(inputValue.toLocaleLowerCase())).slice(0, 3); 
     }
     return projects?.slice(0, 3);
   }, [inputValue]);
 
 
-
   let recentSearch = JSON.parse(localStorage.getItem('history'));
-  console.log(recentSearch)
-  
-  recentSearch = useMemo(() => {
-    let filterSearch = recentSearch?.reverse().filter(search => search?.toLowerCase() === inputValue?.toLowerCase());
-    console.log(filterSearch)
-    recentSearch = recentSearch?.filter(search => !filterSearch?.includes(search));
-    recentSearch = [filterSearch[0]]?.concat(recentSearch).filter(el => el);
-    return recentSearch
-  }, [recentSearch])
-
-  // localStorage.setItem('history', JSON.stringify(recentSearch));
-  
-  
-  console.log(recentSearch)
 
   
   return (
